77dec7e16987487265eab5f46fb3b173009081ee
Mariano Hidalgo <mhidalgo@fi.uba.ar>
2026-07-24 18:38:05 -0300

    refactor:restricciones not null y unique agregadas

--- init.sql
+++ init.sql
@@ -1,46 +1,46 @@
 create table NAVE (Nave_id SERIAL primary key, 
-Modelo VARCHAR(30),
+Modelo VARCHAR(30) NOT NULL,
 Tiempo_de_uso INT,
 Anio_fabricacion INT,
-Kilometraje_recorrido_ BIGINT, 
-Capacidad_max_pasajeros INT,
-Estado VARCHAR(20));
+Kilometraje_recorrido BIGINT, 
+Capacidad_max_pasajeros INT NOT NULL CHECK(Capacidad_max_pasajeros>0),
+Estado VARCHAR(20) NOT NULL);
 
 create table PASAJEROS (Pasajero_id SERIAL primary key,
-Documento VARCHAR(20),
-Nombre VARCHAR(40),
-Apellido VARCHAR(40),
-Edad INT,
+Documento VARCHAR(20) NOT NULL UNIQUE,
+Nombre VARCHAR(40) NOT NULL,
+Apellido VARCHAR(40) NOT NULL,
+Edad INT NOT NULL CHECK(Edad>=0),
 Telefono VARCHAR(20),
-Estado_salud VARCHAR(30),
+Estado_salud VARCHAR(30) NOT NULL,
 Direccion VARCHAR(30));
 
 create table PLATAFORMA(Plataforma_id SERIAL primary key,
-Pais VARCHAR(30),
-Latitud DECIMAL(10,6),
-Longitud DECIMAL(10,6),
-Capacidad_max_naves INT,
-Estado_plataforma VARCHAR(30));
+Pais VARCHAR(30) NOT NULL,
+Latitud DECIMAL(10,6) NOT NULL,
+Longitud DECIMAL(10,6) NOT NULL,
+Capacidad_max_naves INT NOT NULL CHECK (Capacidad_max_naves>0),
+Estado_plataforma VARCHAR(30)NOT NULL);
 
 create table VIAJE (Viaje_id SERIAL primary key,
-Fecha_despegue DATE,
-Horario_salida TIME,
-Duracion INT,
-Estado_despegues VARCHAR(30),
-Plataforma_origen_id INT, 
-foreign key (Plataforma_origen_id) references PLATAFORMA(Plataforma_id),
-Plataforma_destino_id INT, 
-foreign key (Plataforma_destino_id) references PLATAFORMA(Plataforma_id),
+Fecha_despegue DATE NOT NULL,
+Horario_salida TIME NOT NULL,
+Duracion INT NOT NULL CHECK (Duracion>0),
+Estado_despegues VARCHAR(30) NOT NULL,
+Plataforma_origen_id INT NOT NULL, 
+Plataforma_destino_id INT NOT NULL, 
 Naves_id INT,
+foreign key (Plataforma_origen_id) references PLATAFORMA(Plataforma_id),
+foreign key (Plataforma_destino_id) references PLATAFORMA(Plataforma_id),
 foreign key (Naves_id) references NAVE(Nave_id)
 );
 
 create table RESERVA (Reserva_id SERIAL primary key,
-Codigo_asiento VARCHAR(10),
-Fecha_reserva DATE,
-Precio_pasaje DECIMAL(10,6),
-Vuelo_id INT, 
+Codigo_asiento VARCHAR(10)NOT NULL,
+Fecha_reserva DATE NOT NULL,
+Precio_pasaje DECIMAL(10,6) NOT NULL CHECK (Precio_pasaje>=0),
+Vuelo_id INT NOT NULL, 
+Pasajero_id INT NOT NULL,
 foreign key (Vuelo_id) references VIAJE(Viaje_id),
-Pasajero_id INT,
 foreign key (Pasajero_id) references PASAJEROS(Pasajero_id)
 );
