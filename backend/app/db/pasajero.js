6fedf607162008a229c0900ae09b806f153b7c5e
dtorod-hn <dtorod@fi.uba.ar>
2026-08-03 13:20:05 -0300

    Update pasajero.js

--- backend/app/db/pasajero.js
+++ backend/app/db/pasajero.js
@@ -17,7 +17,7 @@ export async function obtenerUnPasajero(id){
 // Para agregar un pasajero
 export async function agregarPasajero(documento, nombre, apellido, edad, telefono, salud, direccion){
     const query = "INSERT INTO pasajeros (Documento, Nombre, Apellido, Edad, Telefono, Estado_salud, Direccion) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);";
-    const res = await db.query(query, [documento, nombre, apellido, edad, telefono, telefono, salud, direccion]);
+    const res = await db.query(query, [documento, nombre, apellido, edad, telefono, salud, direccion]);
     return res.rowCount == 1;
 }
 
