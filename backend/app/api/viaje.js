a5d55efed6a590fabde8357e7ab8cf8e67261524
Mariano Hidalgo <mhidalgo@fi.uba.ar>
2026-08-05 22:40:47 -0300

    feat: agregue reglas de negocio para reserva y viajes

--- backend/app/api/viaje.js
+++ backend/app/api/viaje.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { actualizarViaje, agregarViaje, eliminarViaje, obtenerTodosViajes, obtenerCantidadViajes, obtenerUnViaje } from '../db/viaje.js';
+import { obtenerUnaNave } from '../db/nave.js';
+import { obtenerUnaPlataforma } from '../db/plataforma.js';
 
 export const endpointsViaje = Router();
 
@@ -13,28 +15,55 @@ endpointsViaje.get("/count", async (req, res) => {
 endpointsViaje.get("/", async (req, res) => {
     const viajes = await obtenerTodosViajes();
     res.json(viajes);
-}) 
+})
 
 // Para obtener un solo viaje
 endpointsViaje.get("/:id", async (req, res) => {
     let id = req.params.id;
     const viaje = await obtenerUnViaje(id);
     res.json(viaje);
-}) 
+})
 
 // Para agregar un viaje
 endpointsViaje.post("/", async (req, res) => {
-    const viaje = await agregarViaje(
-        req.body.fecha,
-        req.body.horario,
-        req.body.duracion,
-        req.body.estado,
-        req.body.plataforma_origen,
-        req.body.plataforma_destino,
-        req.body.naves
-    );
-    res.status(201).json({message: "Viaje agregado."});
-}) 
+    try {
+        const id_nave = req.body.naves;
+        const nave = await obtenerUnaNave(id_nave);
+        if (!nave || nave.estado !== "operativa") {
+            return res.status(400).json({
+                message: "Operación rechada. La nave no está operando en este momento"
+            });
+        }
+
+        const plataforma_origen = await obtenerUnaPlataforma(req.body.plataforma_origen);
+        const plataforma_destino = await obtenerUnaPlataforma(req.body.plataforma_destino)
+        if (!plataforma_origen || plataforma_origen.estado !== "operativa") {
+            return res.status(400).json({
+                message: "Operación rechazada. La plataforma de origen no está operando en este momento"
+            });
+        }
+
+        if (!plataforma_destino || plataforma_destino.estado !== "operativa") {
+            return res.status(400).json({
+                message: "Operación rechazada. La plataforma de destino no está operando en este momento"
+            });
+        }
+
+        await agregarViaje(
+            req.body.fecha,
+            req.body.horario,
+            req.body.duracion,
+            req.body.estado,
+            req.body.plataforma_origen,
+            req.body.plataforma_destino,
+            req.body.naves
+        );
+        res.status(201).json({ message: "Viaje agregado." });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error del servidor" })
+    }
+})
 
 // Para actualizar un viaje
 endpointsViaje.put("/:id", async (req, res) => {
@@ -61,6 +90,6 @@ endpointsViaje.delete("/", async (req, res) => {
     const viaje = await eliminarViaje(
         req.body.id
     );
-    res.status(200).json({message: "Viaje eliminado."});
+    res.status(200).json({ message: "Viaje eliminado." });
 })
 
